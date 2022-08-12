3b84ff71acc43357be6069bd621088c54f81b41d
xphn95 <843361695@qq.com>
2022-08-12 18:13:56 +0800

    add auto import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Unocss from 'unocss/vite';
+import AutoImport from 'unplugin-auto-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue({ reactivityTransform: true }),
+    Unocss(),
+    AutoImport({
+      imports: ['vue', 'vue/macros'],
+      dts: true,
+      vueTemplate: true,
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
 });
