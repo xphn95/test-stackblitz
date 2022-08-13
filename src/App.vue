2e9d50574e34a7353906e224b24514c7dac5398e
xphn95 <843361695@qq.com>
2022-08-13 23:40:45 +0800

    try echarts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <script setup>
-const
+const msg = $ref('hello')
+
+// echarts标准格式
+const echartInit = () => {
+  //获取DOM元素并初始化
+  var myChart = echartsInit(document.getElementById('main'))
+  // 指定图表的配置项和数据
+  var option = {
+    //这里的yAxis就是竖轴，xAxis就是横轴
+    // yAxis and xAxis 交换可以改变横向或竖向
+    yAxis: {
+      data: ['jack', 'blob', 'lucy'],
+    },
+    xAxis: {},
+    // 数据的来源
+    series: [
+      {
+        name: 'source',
+        // bar就是柱状图
+        type: 'bar',
+        // 数据
+        data: [10, 20, 30],
+      },
+    ],
+  }
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option)
+}
+
+// 这里需要注意，不能在setup内执行getElementById操作，因为setup是在文档加载完毕之前执行，那么
+// 取到的一定是null导致init失败，所以需要在onMounted中执行
+onMounted(() => {
+  echartInit()
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div text="center pink-300 5">
+    {{ msg }}
   </div>
+  <div id="main" style="width: 100%; height: 400px"></div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import Unocss from 'unocss/vite';
-import AutoImport from 'unplugin-auto-import';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({ reactivityTransform: true }),
-    Unocss(),
+    Unocss({
+      mode: 'vue-scoped',
+    }),
     AutoImport({
-      imports: ['vue', 'vue/macros'],
+      imports: [
+        'vue',
+        'vue/macros',
+        {
+          echarts: [['init', 'echartsInit']],
+        },
+      ],
       dts: true,
       vueTemplate: true,
       eslintrc: {
@@ -17,4 +25,4 @@ export default defineConfig({
       },
     }),
   ],
-});
+})
